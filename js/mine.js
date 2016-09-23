b3ac98423f0decf4552dc36ee9188aa924bceeb2
behzad <khosrojerdi.behzad1@gmail.com>
2016-09-23 19:42:13 +0330

    a

--- js/mine.js
+++ js/mine.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-    jQuery('.header').load('./header.html');
-    jQuery('.footer').load('./footer.html');
+	$("#body").load("./pages/default.html");
 });
 
+// Fired once when document is ready
+//$(document).one('load', function () {
+//	$("#body").load("./pages/default.html");
+//});
+
+function contact() {
+	$("#body").load("./pages/contact.html");
+}
+function resume() {
+	$("#body").load("./pages/resume.html");
+}
+
+function home() {
+	$("#body").load("./pages/default.html");
+}
